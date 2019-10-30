40c31d1a011da6f7f517d3f617b366be98263bf5
Gabriel Previato <gabriel.previato@gmail.com>
2019-10-29 22:48:49 -0300

    Add files via upload

--- trabalho3.m
+++ trabalho3.m
@@ -4,26 +4,30 @@
 #          Pedro Souza      RA223515
 #
 
+#function y=RNA(alpha,W,b,x)
+#  y=alpha*tanh(W*x+b);
+#endfunction
+
 load DadosTreinamento.mat
 
 # Xtr
 d = 30;
 m = 393;
-
+n = 20;
 x = Xtr;
 y = ytr;
-W = randn(m, d);
-b = randn(m, 1);
+W = randn(n, d);
+b = randn(n, 1);
 
 #function retval = g(w, x, b):
 #  retval = tanh(W*x+b);
 #endfunction
 G = tanh(W*Xtr+b);
-A = zeros(d, m);
-b = zeros(m);
+A = zeros(n, n);
+B = zeros(n,1);
 
-for i = 1:d
-  for j = 1:m
+for i = 1:n
+  for j = 1:n
     sum = 0;
     for k = 1:m
       sum = sum + G(i, k)*G(j, k);
@@ -36,5 +40,30 @@ for i = 1:d
   for k = 1:m
     sum = sum + G(i, k)*y(k);
   end
-  b(i) = sum;
+  B(i) = sum;
 end
+
+alpha = A \ B;
+
+result_2 = alpha'*tanh(W*x+b);
+
+function ret = gr(L, y, result_2)
+  false_negative = 0;
+  malignos_true = 0;
+  for i = 1:length(y)
+    if y(i) == 1 && result_2(i) <= L
+      false_negative = false_negative + 1;
+    endif
+    
+    if y(i) == 1
+      malignos_true = malignos_true + 1;
+    endif
+  end
+  
+  ret = false_negative / malignos_true
+endfunction
+
+result = zeros(200, 1)
+for i = 1:200
+  result(i) = gr((i/100 - 1), ytr, result_2)
+endfor
